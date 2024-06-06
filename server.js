6a8fe973a0fa40b715f7e89c064a8aac48a8398a
atengelsgjerd <atengelsgjerd@gmail.com>
2024-06-05 20:16:28 -0500

    created initial folders/files

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const app = express();
+const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
+const routes = require('./controllers/routes');
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
