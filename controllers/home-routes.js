def57f226c2658bb5b3ac1cf6462c67d5b54d490
Arnaud Tiam <arnaudtiam50@gmail.com>
2024-06-12 19:00:30 -0500

    Make lesson display on the correct order.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,10 +6,32 @@ router.get('/', withEmail, async (req, res) => {
 
     try {
         const moduleData = await Module.findAll({
-            include: [Lessons]
+            include: [{ 
+                model: Lessons                
+            },
+
+        ],
+
+        order: [
+            [Lessons, 'id', 'ASC']
+        ]
         });
         const modules = moduleData.map((module) => module.get({ plain: true }));
-        console.log("modules", modules);
+        console.log("modules_lessons", modules[0].lessons);
+        // modules[0].lessons.sort((a, b) => {
+        //     if (a[0] !== b[0]) {
+        //         return a[0] - b[0];
+        //     }
+        //     if (a[1] < b[1]) {
+        //         return -1;
+        //     }
+        //     if (a[1] > b[1]) {
+        //         return 1;
+        //     }
+        //     return 0; // second elements are equal
+        // // });
+        // console.log(lessons);;
+
 
         res.render('home', { 
             modules: modules,
